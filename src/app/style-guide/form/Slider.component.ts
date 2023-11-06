f49967acaa15d86acffb6473a7779834c249e359
윤기오 <5dogs@naver.com>
2023-11-06 12:50:50 +0900

    Slider

--- src/app/style-guide/form/Slider.component.ts
+++ src/app/style-guide/form/Slider.component.ts
@@ -10,13 +10,10 @@ import {
 import { TranslateService } from '@ngx-translate/core';
 import { Router } from '@angular/router';
 import { Options } from 'ngx-slider-v2';
+import * as moment from 'moment';
+import * as numeral from 'numeral';
+import * as _ from 'lodash';
 
-interface SliderDetails {
-  value: number;
-  highValue: number;
-  floor: number;
-  ceil: number;
-}
 /**
  * @class StyleGuideButtonComponent *
  * */
@@ -45,6 +42,11 @@ export class StyleGuideSliderComponent implements AfterViewInit, OnDestroy {
       desc: 'Slider Step',
     },
     {
+      title: 'Slider 이하',
+      anchor: 'sliderLess',
+      desc: 'Slider 이하',
+    },
+    {
       title: 'Slider 이상',
       anchor: 'sliderGreater',
       desc: 'Slider 이상',
@@ -60,48 +62,65 @@ export class StyleGuideSliderComponent implements AfterViewInit, OnDestroy {
       desc: 'Slider 날짜',
     }
   ];
+  singleValue = ['basicSlider', 'sliderStep', 'sliderLess', 'sliderGreater'];
   value: number = 100;
   options: Options = {
     floor: 0,
     ceil: 250
   };
 
-  sliders: SliderDetails[] = [
-    {
-      value: -1,
-      highValue: 2,
-      floor: -5,
-      ceil: 5
-    },
-    {
-      value: 1,
-      highValue: 2,
+  sliders: any[] = [
+    { // 기본
+      value: 10,
       floor: 0,
-      ceil: 5
+      ceil: 50
     },
-    {
-      value: 30,
-      highValue: 60,
+    { // 범위
+      value: 10,
+      highValue: 20,
       floor: 0,
       ceil: 100
     },
-    {
+    { // 스텝
       value: 30,
       highValue: 60,
       floor: 0,
+      step: 5,
       ceil: 100
     },
-    {
+    { // 이하
       value: 30,
       highValue: 60,
       floor: 0,
-      ceil: 100
+      ceil: 100,
+      showSelectionBar: true
     },
-    {
+    { // 이상
       value: 30,
       highValue: 60,
       floor: 0,
-      ceil: 100
+      ceil: 100,
+      showSelectionBarEnd: true
+    },
+    { // Format
+      value: -3000,
+      highValue: 6000,
+      floor: -10000,
+      ceil: 10000,
+      step: 100,
+      translate: (value: number): string => {
+        return numeral(value).format('0.0a');
+      }
+    },
+    { // 날짜
+      value: Number(moment().subtract(1, 'months').format('x')),
+      highValue: Number(moment().format('x')),
+      floor: Number(moment().subtract(1, 'months').format('x')),
+      ceil: Number(moment().format('x')),
+      step: 1000 * 60 * 60, // 최소 분 기준
+      translate: (value: number): string => {
+        return moment(value).format('YYYY.MM.DD hh:mm');
+      }
     }
   ];
   constructor(
@@ -114,10 +133,12 @@ export class StyleGuideSliderComponent implements AfterViewInit, OnDestroy {
     this.cd.detectChanges();
   }
 
-  sliderOptions(slider: SliderDetails): Options {
+  sliderOptions(slider: any): Options {
+    const option = _.cloneDeep(slider);
+    delete option.value;
+    delete option.highValue;
     return {
-      floor: slider.floor,
-      ceil: slider.ceil
+      ...option
     };
   }
   ngOnDestroy(): void { }
