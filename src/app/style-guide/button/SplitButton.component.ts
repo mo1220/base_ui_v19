ecf3e45f9737a425bc2c28ea8aada22bb53bdde7
jerry <daisy@datatree.co.kr>
2023-11-27 11:10:53 +0900

    split button 수정

--- src/app/style-guide/button/SplitButton.component.ts
+++ src/app/style-guide/button/SplitButton.component.ts
@@ -23,17 +23,48 @@ import {menuType} from "../style-guide.models";
 })
 export class StyleGuideSplitButtonComponent implements AfterViewInit, OnDestroy {
   // MenuS
+  colors = [
+    {
+      color: 'primary',
+      label: 'Primary'
+    },
+    {
+      color: 'secondary',
+      label: 'Secondary'
+    },
+    {
+      color: 'success',
+      label: 'Success'
+    },
+    {
+      color: 'info',
+      label: 'Info'
+    },
+    {
+      color: 'danger',
+      label: 'Danger'
+    },
+    {
+      color: 'light',
+      label: 'Light'
+    },
+    {
+      color: 'dark',
+      label: 'Dark'
+    }
+  ]
   splitMenu: Array<menuType> = [
     {
       title: 'Basic',
       anchor: 'basic',
       desc: '기본 split 버튼 <br/>' +
-        '<span class="msg info font-size-xs">화살표 버튼 클릭 참고 : 아이콘 버튼 cursor default 주기 위해 <b>label.btn.btn-split > span</b></span>'
+        '<span class="msg info">화살표 버튼 클릭 참고 : 아이콘 버튼 cursor default 주기 위해 <b>label.btn.btn-split > span</b></span>'
     },
     {
       title: 'Position',
       anchor: 'position',
-      desc: '위치별 split 버튼'
+      desc: '위치별 split 버튼 <br/>' +
+        '<span class="msg info"><b>yPosition</b>에 above, below 입력 <b>xPosition</b>에 before, after 입력</span>'
     },
     {
       title: 'Color Type',
@@ -44,6 +75,11 @@ export class StyleGuideSplitButtonComponent implements AfterViewInit, OnDestroy 
       title: 'Color Outline Type',
       anchor: 'colorOutlineType',
       desc: '컬러별 라인 split 버튼'
+    },
+    {
+      title: 'Divider',
+      anchor: 'divider',
+      desc: '아이콘 버튼에 구분선 추가'
     },
     {
       title: 'Size',
@@ -84,34 +120,34 @@ export class StyleGuideSplitButtonComponent implements AfterViewInit, OnDestroy 
     this.notifiService.default(message);
   }
 
-  onAnchor(activeIdx: number){
-    this.scrolling = true;
-    this.activeNum = activeIdx;
-    const targetY = this.anchors.get(activeIdx)?.nativeElement?.offsetTop;
-    window.scrollTo({left: 0, top: targetY, behavior: 'smooth'});
-  }
-
-  contentScroll(scrollTop: number): void{
-    if (!this.scrolling) {
-      for(let i=0; i<this.splitMenu.length-1; i++){
-        const from = this.anchors.get(i)?.nativeElement.offsetTop - 50;
-        const to = this.anchors.get(i+1)?.nativeElement.offsetTop - 50;
-        if(scrollTop < to && scrollTop > from){
-          this.activeNum = i;
-          break;
-        }
-      }
-    }
-  }
-
-  @HostListener('window:scroll', ['$event']) // for window scroll events
-  onWindowScroll(event: any) {
-    event.stopPropagation();
-    this.contentScroll(window.scrollY);
-  }
-  @HostListener('window:scrollend', ['$event']) // for window scroll events
-  onWindowScrollEnd(event: any) {
-    event.stopPropagation();
-    this.scrolling = false;
-  }
+  // onAnchor(activeIdx: number){
+  //   this.scrolling = true;
+  //   this.activeNum = activeIdx;
+  //   const targetY = this.anchors.get(activeIdx)?.nativeElement?.offsetTop;
+  //   window.scrollTo({left: 0, top: targetY, behavior: 'smooth'});
+  // }
+  //
+  // contentScroll(scrollTop: number): void{
+  //   if (!this.scrolling) {
+  //     for(let i=0; i<this.splitMenu.length-1; i++){
+  //       const from = this.anchors.get(i)?.nativeElement.offsetTop - 50;
+  //       const to = this.anchors.get(i+1)?.nativeElement.offsetTop - 50;
+  //       if(scrollTop < to && scrollTop > from){
+  //         this.activeNum = i;
+  //         break;
+  //       }
+  //     }
+  //   }
+  // }
+  //
+  // @HostListener('window:scroll', ['$event']) // for window scroll events
+  // onWindowScroll(event: any) {
+  //   event.stopPropagation();
+  //   this.contentScroll(window.scrollY);
+  // }
+  // @HostListener('window:scrollend', ['$event']) // for window scroll events
+  // onWindowScrollEnd(event: any) {
+  //   event.stopPropagation();
+  //   this.scrolling = false;
+  // }
 }
