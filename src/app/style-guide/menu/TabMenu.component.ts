f69b5d6437173cb6cd12d1073cfe919046989a4c
miki <mulberry@datatree.co.kr>
2023-12-28 10:26:27 +0900

    tab select event 추가 ngx-tab custom header

--- src/app/style-guide/menu/TabMenu.component.ts
+++ src/app/style-guide/menu/TabMenu.component.ts
@@ -1,6 +1,7 @@
 import {AfterViewInit, ChangeDetectorRef, Component, OnDestroy, ViewEncapsulation} from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Router } from '@angular/router';
+import {TabDirective} from "ngx-bootstrap/tabs";
 
 /**
  * @class StyleGuideButtonComponent *
@@ -11,15 +12,30 @@ import { Router } from '@angular/router';
   encapsulation: ViewEncapsulation.None
 })
 export class StyleGuideTabMenuComponent implements AfterViewInit, OnDestroy {
+
+  matSelectedIndex:number;
+  ngxSelectTab:string;
+
   constructor(
     private cd: ChangeDetectorRef,
     private translate: TranslateService,
     private router: Router
-  ) { }
+  ) {
+  }
 
   ngAfterViewInit(): void {
     this.cd.detectChanges();
   }
 
   ngOnDestroy(): void { }
+
+  changeTabIndex(e: number) {
+    console.log(e);
+    this.matSelectedIndex = e;
+  }
+
+  onSelectTab(data: TabDirective) {
+    console.log(data);
+    this.ngxSelectTab = data.id+'';
+  }
 }
